c1f7f9c8b4c4fb6d9a9d109452ecaaa2ced01bf0
Fatih Ustok <fustok@netas.com.tr>
2020-02-06 10:58:41 +0300

    Make deploy generic

--- cno/deploy.sh
+++ cno/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd ./5gmedia-cno/cno/results && sudo scp -i ~/ValidationExtractService/5gmedia-cno/cno/5g-media-keypair-external.pem $1 ubuntu@217.172.12.203:~/cno_rl_uc2
+cd ./5gmedia-cno/cno/results && sudo scp -i $1 $2 $3
 
 
